3cf67250b7e30bc9138adf6525f88efe87a01bdd
nyan-win-naing-19 <s4115096@rmit.edu.vn>
2025-07-13 21:01:25 +0700

    Mongo Atlas DB Set Up

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,23 @@
-const express = require('express');
-require('dotenv').config();
-const morgan = require('morgan');
-const recipeRoutes = require('./routes/recipes');
+const express = require("express");
+require("dotenv").config();
+const morgan = require("morgan");
+const recipeRoutes = require("./routes/recipes");
+const mongoose = require("mongoose");
 
 const app = express();
+const mongoURL =
+  "mongodb+srv://nyanwinnaing1922002:test1234@mern-cluster.v1weskt.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Cluster";
+mongoose.connect(mongoURL).then(() => {
+  console.log("connected to db");
+  app.listen(process.env.PORT, () => {
+    console.log("app is running on localhost:" + process.env.PORT);
+  });
+});
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-    return res.json({hello: "world"});
+app.get("/", (req, res) => {
+  return res.json({ hello: "world" });
 });
 
 app.use("/api/recipes", recipeRoutes);
-
-app.listen(process.env.PORT, () => {
-    console.log("app is running on localhost:" + process.env.PORT);
-})
