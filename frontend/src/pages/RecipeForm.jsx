4841369770dd818d934479d5ff59dfec4ed016af
nyan-win-naing-19 <s4115096@rmit.edu.vn>
2025-07-14 21:52:46 +0700

    Create Recipe Feature

--- frontend/src/pages/RecipeForm.jsx
+++ frontend/src/pages/RecipeForm.jsx
@@ -2,14 +2,41 @@ import React, { useState } from "react";
 // @ts-ignore
 import plus from "../assets/plus.svg";
 import Ingredients from "../components/Ingredients.jsx";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function RecipeForm() {
+  let navigate = useNavigate();
   let [ingredients, setIngredients] = useState([]);
+  let [title, setTitle] = useState("");
+  let [description, setDescription] = useState("");
   let [newIngredient, setNewIngredient] = useState("");
+  let [errors, setErrors] = useState([]);
 
   let addIngredient = () => {
-    setIngredients(prev => [newIngredient, ...prev]);
-    setNewIngredient('');
+    setIngredients((prev) => [newIngredient, ...prev]);
+    setNewIngredient("");
+  };
+
+  let createRecipe = async (e) => {
+    try {
+      e.preventDefault();
+
+      let recipe = {
+        title,
+        description,
+        ingredients,
+      };
+
+      /// server request
+      let res = await axios.post("http://localhost:8000/api/recipes", recipe);
+      if (res.status === 200) {
+        navigate("/");
+      }
+      console.log(res);
+    } catch (e) {
+      setErrors(Object.keys(e.response.data.errors));
+    }
   };
 
   return (
@@ -17,13 +44,25 @@ export default function RecipeForm() {
       <h1 className="mb-6 text-2xl font-bold text-orange-400 text-center">
         Recipe Create Form
       </h1>
-      <form action="" className="space-y-5">
+      <form action="" className="space-y-5" onSubmit={createRecipe}>
+        <ul className="list-disc pl-3">
+          {!!errors.length &&
+            errors.map((error, i) => (
+              <li className="text-red-500 text-sm" key={i}>
+                {error} is invalid value
+              </li>
+            ))}
+        </ul>
         <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
           type="text"
           placeholder="Recipe Title"
           className="w-full p-1 bg-white"
         />
         <textarea
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
           placeholder="Recipe Description"
           rows={5}
           className="w-full p-1 bg-white"
@@ -36,12 +75,20 @@ export default function RecipeForm() {
             placeholder="Recipe Ingredient"
             className="w-full p-1 bg-white"
           />
-          <img src={plus} alt="" className="cursor-pointer" onClick={addIngredient} />
+          <img
+            src={plus}
+            alt=""
+            className="cursor-pointer"
+            onClick={addIngredient}
+          />
         </div>
         <div>
           <Ingredients ingredients={ingredients} />
         </div>
-        <button className="w-full px-3 py-1 rounded-full bg-orange-400 text-white">
+        <button
+          type="submit"
+          className="w-full px-3 py-1 rounded-full bg-orange-400 text-white"
+        >
           Create Recipe
         </button>
       </form>
