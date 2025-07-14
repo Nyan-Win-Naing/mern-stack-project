34b172373647dc96966042af5a4072bc46937225
nyan-win-naing-19 <s4115096@rmit.edu.vn>
2025-07-14 19:26:12 +0700

    Fetch All Recipes

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const morgan = require("morgan");
 const recipeRoutes = require("./routes/recipes");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 const mongoURL =
@@ -14,6 +15,7 @@ mongoose.connect(mongoURL).then(() => {
   });
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
