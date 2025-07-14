dcaf33e4b6d7d8b96755d8bbc1701f54c1eba06a
nyan-win-naing-19 <s4115096@rmit.edu.vn>
2025-07-14 23:02:00 +0700

    Pagination Frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,40 @@
 import React, { useEffect, useState } from "react";
 import RecipeCard from "../components/RecipeCard";
+import Pagination from "../components/Pagination";
+import { useLocation } from "react-router-dom";
 
 export default function Home() {
   let [recipes, setRecipes] = useState([]);
+  let location = useLocation();
+  let searchQuery = new URLSearchParams(location.search);
+  let page = searchQuery.get("page");
+
+  console.log(page);
 
   useEffect(() => {
     let fetchRecipes = async () => {
-      let response = await fetch("http://localhost:8000/api/recipes");
+      let response = await fetch(
+        "http://localhost:8000/api/recipes?page=" + page
+      );
       if (response.ok) {
         let data = await response.json();
         setRecipes(data);
+
+        // scroll to top
+        window.scroll({ top: 0, left: 0, behavior: "smooth" });
       }
     };
 
     fetchRecipes();
-  }, []);
+  }, [page]);
+
+  /// backend info(hardcode)
+  let links = {
+    nextPage: true,
+    previousPage: false,
+    currentPage: 1,
+    loopableLinks: [{ number: 1 }, { number: 2 }, { number: 3 }],
+  };
 
   return (
     <div className="space-y-3">
@@ -22,6 +42,8 @@ export default function Home() {
         recipes.map((recipe) => (
           <RecipeCard recipe={recipe} key={recipe._id} />
         ))}
+
+      <Pagination links={links} page={page} />
     </div>
   );
 }
