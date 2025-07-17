abe56c4a54d14bf01ff77c42cae6a67adacb1282
nyan-win-naing-19 <s4115096@rmit.edu.vn>
2025-07-18 00:22:38 +0700

    Register User API

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,12 +1,32 @@
-import User from "../models/User";
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 const UserController = {
-    login: (req, res) => {
-        return res.json({msg: "user login api hit"});
-    },
-    register: (req, res) => {
-        return res.json({msg: "user register api hit"});
+  login: (req, res) => {
+    return res.json({ msg: "user login api hit" });
+  },
+  register: async (req, res) => {
+    try {
+      let { name, email, password } = req.body;
+      let userExists = await User.findOne({ email });
+      if (userExists) {
+        throw new Error("user already exists");
+      }
+
+      let salt = await bcrypt.genSalt();
+      let hashValue = await bcrypt.hash(password, salt);
+
+      let user = await User.create({
+        name,
+        email,
+        password: hashValue,
+      });
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ msg: e.message });
     }
+  },
 };
 
 module.exports = UserController;
