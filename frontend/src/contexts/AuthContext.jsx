87586bf87ec82661403c2cf1ec598bacea822d6c
nyan-win-naing-19 <s4115096@rmit.edu.vn>
2025-08-03 01:06:39 +0700

    Member LogIn User State

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,15 +1,17 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const AuthContext = createContext();
 
 let AuthReducer = (state, action) => {
   switch (action.type) {
     case "LOGIN":
-      
-      return {user: action.payload};
+      // store user in localstorage
+      localStorage.setItem("user", JSON.stringify(action.payload));
+      return { user: action.payload };
     case "LOGOUT":
-      console.log("action hit logout", action.payload);
-      return {user: null};
+      // remove user in localstorage
+      localStorage.removeItem("user");
+      return { user: null };
     default:
       return state;
   }
@@ -20,7 +22,24 @@ const AuthContextProvider = ({ children }) => {
     user: null,
   });
 
-  return <AuthContext.Provider value={{...state, dispatch}}>{children}</AuthContext.Provider>;
+  useEffect(() => {
+    try {
+      let user = JSON.parse(localStorage.getItem("user"));
+      if (user) {
+        dispatch({ type: "LOGIN", payload: user });
+      } else {
+        dispatch({ type: "LOGOUT" });
+      }
+    } catch (e) {
+      dispatch({ type: "LOGOUT" });
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export { AuthContext, AuthContextProvider };
